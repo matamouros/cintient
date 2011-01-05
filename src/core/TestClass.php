6d525ccdca43f2bd7c95c911ae8d05138a8838f0
Pedro Mata-Mouros <pedro.matamouros@gmail.com>
2011-01-05 00:04:31 +0000

    Helper methods for handling Junit XML.

---
+++ src/core/TestClass.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * Cintient, Continuous Integration made simple.
+ * 
+ * Copyright (c) 2011, Pedro Mata-Mouros <pedro.matamouros@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * . Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * 
+ * . Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *   
+ * . Neither the name of Pedro Mata-Mouros Fonseca, Cintient, nor
+ *   the names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *   
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+/**
+ * Helper class for dealing with Junit test suites (at the file level)
+ */
+class TestClass
+{
+  private $_name;
+  private $_file;
+  private $_tests;
+  private $_assertions;
+  private $_failures;
+  private $_errors;
+  private $_time;
+  private $_testMethods;
+  
+  /**
+   * Magic method implementation for calling vanilla getters and setters. This
+   * is rigged to work only with private/protected non-static class variables
+   * whose nomenclature follows the Zend Coding Standard.
+   * 
+   * @param $name
+   * @param $args
+   */
+  public function __call($name, $args)
+  {
+    if (strpos($name, 'get') === 0) {
+      $var = '_' . lcfirst(substr($name, 3));
+      return $this->$var;
+    } elseif (strpos($name, 'set') === 0) {
+      $var = '_' . lcfirst(substr($name, 3));
+      $this->$var = $args[0];
+      return true;
+    } else {
+      trigger_error("No valid method available for calling", E_USER_ERROR);
+      exit;
+    }
+    return false;
+  }
+}
