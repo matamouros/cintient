0ea1a6a28fa6150c5a3deac702b8ab04dbdc5d3f
Pedro Mata-Mouros <pedro.matamouros@gmail.com>
2011-02-14 18:27:52 +0000

    Enough with this renaming crap

---
+++ src/handlers/installHandler.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * 
+ *  Cintient, Continuous Integration made simple.
+ *  Copyright (c) 2010, 2011, Pedro Mata-Mouros Fonseca
+ *
+ *  This file is part of Cintient.
+ *
+ *  Cintient is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Cintient is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Cintient. If not, see <http://www.gnu.org/licenses/>.
+ *  
+ */
+
+//
+// Dependencies
+//
+
+// TODO: Check for SQLite3 extension
+extension_loaded('sqlite3'); 
+
+// TODO: Check version compatibility
+//SQLite3::version 
+
+//TODO: Perms on the dir where the database will exist
+
+//TODO: Touch log file and perm it up.
+
+//TODO: Create work dir and perm it up.
