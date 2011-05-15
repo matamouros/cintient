2c5aae3ed97670b361d3376dc0e2786575768469
Pedro Mata-Mouros <pedro.matamouros@gmail.com>
2011-05-15 15:56:12 +0000

    Forget the licensing mambo jambo.

--- src/core/FilesystemFilterIterator.php
+++ src/core/FilesystemFilterIterator.php
@@ -1,4 +1,26 @@
 <?php
+/*
+ * 
+ *  Cintient, Continuous Integration made simple.
+ *  Copyright (c) 2010, 2011, Pedro Mata-Mouros Fonseca
+ *
+ *  This file is part of Cintient.
+ *
+ *  Cintient is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Cintient is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Cintient. If not, see <http://www.gnu.org/licenses/>.
+ *  
+ */
+
 class FilesystemFilterIterator extends FilterIterator
 {
   /**
