918ece8f1a576b2cb8dbedd6f7cd5e509c9970c4
Pedro Mata-Mouros <pedro.matamouros@gmail.com>
2011-11-20 23:40:14 +0000

    Query for getting next projects to build had a flaw in the datetime arithmetics.

--- src/core/Project.php
+++ src/core/Project.php
@@ -774,7 +774,7 @@ EOT;
 
     $ret = array();
     $sql = 'SELECT * FROM project p'
-         . " WHERE datecheckedforchanges < DATETIME('now', -(scmcheckchangestimeout*".CINTIENT_PROJECT_CHECK_CHANGES_TIMEOUT_DEFAULT.") || ' minutes', 'localtime')"
+         . " WHERE datecheckedforchanges < DATETIME('now', -(scmcheckchangestimeout) || ' minutes', 'localtime')"
          . ' ORDER BY datecheckedforchanges ASC LIMIT ?, ?';
     if ($rs = Database::query($sql, array($options['pageStart'], $options['pageLength']))) {
       while ($rs->nextRow()) {
