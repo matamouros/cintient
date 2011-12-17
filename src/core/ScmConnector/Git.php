f5dad5a54c2353197065d8cfff4eb76b0e554934
Pedro Mata-Mouros <pedro.matamouros@gmail.com>
2011-12-17 01:36:01 +0000

    Fixed Git::update() on Windows platforms.

--- src/core/ScmConnector/Git.php
+++ src/core/ScmConnector/Git.php
@@ -113,7 +113,7 @@ class ScmConnector_Git extends ScmConnectorAbstract implements ScmConnectorInter
   public function update(&$rev)
   {
     // We can't use "git --git-dir={$this->getLocal()} pull", it's wrong
-    $command = "cd {$this->getLocal()}; {$GLOBALS['settings'][SystemSettings::EXECUTABLE_GIT]} pull";
+    $command = "cd {$this->getLocal()} && {$GLOBALS['settings'][SystemSettings::EXECUTABLE_GIT]} pull";
     $proc = new Framework_Process();
     $proc->setExecutable($command, false); // false for no escapeshellcmd() (because of the ';')
     $proc->run();
